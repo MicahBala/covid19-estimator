83230dffb9949c7e5eb019be21aedd66d858d505
Micah Bala <micahkulien@gmail.com>
2020-04-10 19:35:25 +0100

    Modified estimator.js to pass challenge 3 audit

--- src/estimator.js
+++ src/estimator.js
@@ -64,14 +64,14 @@ const covid19ImpactEstimator = (data) => {
   let severeCases = getCases(impact.infectionsByRequestedTime, 0.15);
   severeCases = Math.trunc(severeCases);
 
-  let casesForICU = getCases(impact.infectionsByRequestedTime, 0.5);
+  let casesForICU = getCases(impact.infectionsByRequestedTime, 0.05);
   casesForICU = Math.trunc(casesForICU);
 
-  let casesForVentilators = getCases(impact.infectionsByRequestedTime, 0.2);
+  let casesForVentilators = getCases(impact.infectionsByRequestedTime, 0.02);
   casesForVentilators = Math.trunc(casesForVentilators);
 
   const populationIncome = Math.trunc(avgDailyIncomePopulation * avgDailyIncomeInUSD);
-  const moneyLoss = (impact.currentlyInfected * populationIncome) / estimate;
+  const moneyLoss = (impact.infectionsByRequestedTime * populationIncome) / estimate;
 
   let hospitalBeds = getCases(totalHospitalBeds, 0.35);
   hospitalBeds = Math.trunc(hospitalBeds - severeCases);
@@ -88,14 +88,14 @@ const covid19ImpactEstimator = (data) => {
   let severeImpactCases = getCases(severeImpact.infectionsByRequestedTime, 0.15);
   severeImpactCases = Math.trunc(severeImpactCases);
 
-  let severeCasesForICU = getCases(severeImpact.infectionsByRequestedTime, 0.5);
+  let severeCasesForICU = getCases(severeImpact.infectionsByRequestedTime, 0.05);
   severeCasesForICU = Math.trunc(severeCasesForICU);
 
-  let severeCasesForVentilators = getCases(severeImpact.infectionsByRequestedTime, 0.2);
+  let severeCasesForVentilators = getCases(severeImpact.infectionsByRequestedTime, 0.02);
   severeCasesForVentilators = Math.trunc(severeCasesForVentilators);
 
   const severePopIncome = Math.trunc(avgDailyIncomePopulation * avgDailyIncomeInUSD);
-  const severeMoneyLoss = (severeImpact.currentlyInfected * severePopIncome) / estimate;
+  const severeMoneyLoss = (severeImpact.infectionsByRequestedTime * severePopIncome) / estimate;
 
   let severeImpactHospitalBeds = getCases(totalHospitalBeds, 0.35);
   severeImpactHospitalBeds = Math.trunc(severeImpactHospitalBeds - severeImpactCases);
